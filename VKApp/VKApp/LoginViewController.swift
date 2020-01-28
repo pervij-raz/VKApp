f66fc4bd95ee8eaf31cc78dd2654aa44f2823282
Volha Bychok <olgabychok88@gmail.com>
2020-01-28 20:48:41 +0300

    basicUI

---
+++ VKApp/VKApp/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  VKApp
+//
+//  Created by Ольга Бычок on 28/01/2020.
+//  Copyright © 2020 Ольга Бычок. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
